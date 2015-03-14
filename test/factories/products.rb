82b2252a3bb4ae3ecf2246467757d3d07d0ed702
ksjoshi88 <ksjoshi88@gmail.com>
2015-03-15 00:39:19 +0530

    Wrote tests for Product Model using Factory girl

---
+++ test/factories/products.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+
+  factory :product do
+    title "Nokia Lumia 530"
+    description "THis is the latest mobile phone for windows phone 8.1"
+    image_url "lumia.jpg"
+    price 90.00
+
+  end
+
+
+
+end
